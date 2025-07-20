d64b77ddcaf991d3eba381e26429e980d311a20f
Gabriel <gabrielgarrone670@gmail.com>
2025-07-20 19:33:39 +0200

    feat: add header component and integrate into app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Menu } from "./components/menu/menu";
+import { Header } from "./components/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu],
+  imports: [RouterOutlet, Menu, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
