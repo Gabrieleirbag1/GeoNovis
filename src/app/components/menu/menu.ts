a3dd084b29e733c28c98ff6fe2ea9203554eb74d
Gabriel <gabrielgarrone670@gmail.com>
2025-07-21 10:01:18 +0200

    feat: enhance menu component with routing logic and initialization

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.css'
 })
-export class Menu {
+export class Menu implements OnInit {
+  currentRoute: string;
+
+  constructor() {
+    this.currentRoute = window.location.pathname.split('/').slice(-1)[0];
+  }
+
+  ngOnInit() {
+    console.log(this.currentRoute);
+  }
 
 }
