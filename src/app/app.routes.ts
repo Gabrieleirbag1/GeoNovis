5f26d40a9cea73579feaa682d6675f9d99ac217a
Gabriel <gabrielgarrone670@gmail.com>
2025-07-21 16:31:50 +0200

    feat: add Rules component and update routing for improved game functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { Menu } from './components/menu/menu';
 import { Game } from './components/game/game/game';
 import { Home } from './components/home/home';
+import { Rules } from './components/game/rules/rules';
 
 export const routes: Routes = [
-    { path: '', component: Home },
-    { path: 'region', component: Menu },
-    { path: 'gamemode', component: Menu },
-    { path: 'gamemode/capitals', component: Menu },
-    { path: 'gamemode/flags', component: Menu },
-    { path: "game", component: Game },
-    { path: '**', redirectTo: '' },
+  { path: '', component: Home },
+  { path: 'region', component: Menu },
+  { path: 'gamemode', component: Menu },
+  { path: 'gamemode/capitals', component: Menu },
+  { path: 'gamemode/flags', component: Menu },
+  { path: 'game', component: Game },
+  { path: 'rules', component: Rules },
+  { path: '**', redirectTo: '' },
 ];
