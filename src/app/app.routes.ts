61bd18fac7cf409afcf2897688d4f30cff58d230
Gabriel <gabrielgarrone670@gmail.com>
2025-07-21 09:52:44 +0200

    feat: new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Menu } from './components/menu/menu';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Menu},
+    { path: 'region', component: Menu},
+    { path: 'gamemode', component: Menu},
+    { path: 'gamemode/capitals', component: Menu},
+    { path: 'gamemode/flags', component: Menu},
+];
