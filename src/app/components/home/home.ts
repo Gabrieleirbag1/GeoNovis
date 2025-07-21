33018a8670ca6e588e2f612d00322cca7d7ba941
Gabriel <gabrielgarrone670@gmail.com>
2025-07-21 16:51:28 +0200

    feat: implement game container and update home component structure for game functionality

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Menu } from '../menu/menu';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [Menu],
+  imports: [Menu, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit {
+  isGame: boolean = sessionStorage.getItem('isGame') === 'true' ? true : false;
 
+  ngOnInit() {
+
+  }
+
+  createGame() {
+    this.isGame = true;
+    sessionStorage.setItem('isGame', 'true');
+  }
 }
