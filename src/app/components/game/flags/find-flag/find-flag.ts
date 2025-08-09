5f101cc47ceef1acb7bc4e7ce3292d41b738b364
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-08-09 21:36:33 +0200

    feat: implement FindFlag component with game logic and UI; update game template to include FindFlag

--- src/app/components/game/flags/find-flag/find-flag.ts
+++ src/app/components/game/flags/find-flag/find-flag.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameService } from '../../../../services/game.service';
+import { Countries } from '../../../../types/countries.type';
+import { CommonModule } from '@angular/common';
+import { ConvertService } from '../../../../services/convert.service';
+import { GameStateService } from '../../../../services/game-state.service';
 
 @Component({
   selector: 'app-find-flag',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './find-flag.html',
   styleUrl: './find-flag.css'
 })
 export class FindFlag {
+  countries: Countries[] = [];
+  selectedCountry: string = '';
+  endTurn: boolean = false;
+  isCorrect: boolean = false;
+  constructor(private gameService: GameService, private convertService: ConvertService, protected gameStateService: GameStateService) {}
 
+  ngOnInit(): void {
+    console.log('FindCapital Component Initialized');
+    this.gameService.initializeGame(6);
+    this.countries = this.gameService.getCountries();
+    this.selectedCountry = this.convertService.convertCodeToCountry(this.gameService.selectedCountryCode).country[this.convertService.language];
+  }
+
+  checkAnswer(country: any): void {
+    this.isCorrect = this.gameStateService.checkPlayerAnswer(country.code, this.gameService.selectedCountryCode);
+    console.log('Is answer correct?', this.isCorrect);
+    this.endTurn = true;
+  }
 }
--- src/app/components/game/game/game.ts
+++ src/app/components/game/game/game.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { FindCapital } from "../capitals/find-capital/find-capital";
+import { FindFlag } from "../flags/find-flag/find-flag";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
-  imports: [FindCapital],
+  imports: [FindCapital, FindFlag, CommonModule],
   templateUrl: './game.html',
   styleUrl: './game.css'
 })
 export class Game {
-
+  subgamemode = sessionStorage.getItem('subgamemode') || 'findCapital';
 }
