c36e424252fdc27d80ef45ddacc42cae76c710cf
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-08-12 13:40:47 +0200

    feat: initialize currentRound and totalRounds from gameSave data

--- src/app/components/game/game/game.ts
+++ src/app/components/game/game/game.ts
@@ -13,11 +13,15 @@ import { GameSessionService } from '../../../services/game-session.service';
 export class Game implements OnInit{
   gameSave: any;
   subgamemode: string = 'findCapital'; // Example value, can be set dynamically based on game state
+  currentRound: number = 0;
+  totalRounds: number | null = null; // Example value, can be set dynamically based on
   constructor(private gameSessionService: GameSessionService) {}
 
   ngOnInit(): void {
     console.log('Game Component Initialized');
     this.gameSave = this.gameSessionService.getParsedItem('gameSave') || {};
     this.subgamemode = this.gameSave.subgamemode.available[0] || 'findCapital'; // Default to 'findFlag' if not set
+    this.currentRound = this.gameSave.roundState.current || 1; // Default to 1 if not set
+    this.totalRounds = this.gameSave.roundState.total || 50; // Default to 50 if not set
   }
 }
