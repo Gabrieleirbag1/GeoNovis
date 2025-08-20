f5c7bd14b3b02b6d3817aef6c77bf2f97bb148b4
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-08-20 15:26:11 +0200

    feat: add header language selection from from header component

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { LanguageService } from '../../services/language.service';
+import { Language } from '../../types/language.type';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  currentLanguage: Language;
 
+  constructor(private languageService: LanguageService) {
+    this.currentLanguage = this.languageService.getLanguage();
+  }
+
+  selectLanguage(language: Language): void {
+    this.languageService.setLanguage(language);
+    this.currentLanguage = language;
+    // Reload the page to reflect language changes
+    window.location.reload();
+  }
 }
