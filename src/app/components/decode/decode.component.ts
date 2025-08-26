7a0f0be8f1c2918f86bb25064fefcf5573717de6
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-08-26 16:35:32 +0200

    refractor: decode component structure with Promises

--- src/app/components/decode/decode.component.ts
+++ src/app/components/decode/decode.component.ts
@@ -10,16 +10,21 @@ import { GameSessionService } from '../../services/game-session.service';
   styleUrl: './decode.component.css'
 })
 export class DecodeComponent implements OnInit {
-  decodedSessionData: string = '';
+  decodedSessionData: Record<string, string> = {};
   constructor(private apiService: ApiService, private sessionService: GameSessionService) { }
 
   ngOnInit(): void {
     const encodedSessionData = this.getQueryParams();
     if (encodedSessionData) {
-      this.decodeSessionData(encodedSessionData);
-      this.sessionService.setGameSessionData(this.decodedSessionData);
+      this.decodeSessionData(encodedSessionData).then(() => {
+        this.sessionService.setGameSessionData(this.decodedSessionData).then(() => {
+          window.location.href = '/';
+        });
+      })
+      .catch((error) => {
+        console.error('Error decoding session data:', error);
+      });
     } else {
-      // dont redirect on this page
       window.location.href = '/';
     }
   }
@@ -30,19 +35,23 @@ export class DecodeComponent implements OnInit {
     return sessionData;
   }
 
-  private decodeSessionData(encodedSessionData: string): void {
+  private decodeSessionData(encodedSessionData: string): Promise<void> {
     console.log('Encoded data:', encodedSessionData);
     const jsonSessionData = { sessionData: encodedSessionData }
+    return new Promise<void>((resolve, reject) => {
     this.apiService.postSessionDataToDecode(jsonSessionData).subscribe({
       next: (response) => {
         console.log('Session data posted successfully:', response);
         this.decodedSessionData = response.content || 'No content returned';
+        resolve();
       },
       error: (error) => {
         console.error('Error posting session data:', error);
-        this.decodedSessionData = 'Error generating QR code data';
+        this.decodedSessionData = {};
+        reject(error);
       }
     });
+  });
   }
 
 }
