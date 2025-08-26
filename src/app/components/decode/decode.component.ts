2891d5e82791229663fea245c93aa648ec6a7c93
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-08-26 16:13:51 +0200

    feat: decode session data

--- src/app/components/decode/decode.component.ts
+++ src/app/components/decode/decode.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { GameSessionService } from '../../services/game-session.service';
 
 @Component({
   selector: 'app-decode',
@@ -8,16 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './decode.component.css'
 })
 export class DecodeComponent implements OnInit {
-  constructor() { }
+  decodedSessionData: string = '';
+  constructor(private apiService: ApiService, private sessionService: GameSessionService) { }
 
   ngOnInit(): void {
-    this.getQueryParams();
+    const encodedSessionData = this.getQueryParams();
+    if (encodedSessionData) {
+      this.decodeSessionData(encodedSessionData);
+      this.sessionService.setGameSessionData(this.decodedSessionData);
+    } else {
+      // dont redirect on this page
+      window.location.href = '/';
+    }
   }
 
-  private getQueryParams(): void {
+  private getQueryParams(): string | null {
     const queryParams = new URLSearchParams(window.location.search);
-    const data = queryParams.get('data');
-    console.log('Decoded data:', data);
+    const sessionData = queryParams.get('sessionData');
+    return sessionData;
+  }
+
+  private decodeSessionData(encodedSessionData: string): void {
+    console.log('Encoded data:', encodedSessionData);
+    const jsonSessionData = { sessionData: encodedSessionData }
+    this.apiService.postSessionDataToDecode(jsonSessionData).subscribe({
+      next: (response) => {
+        console.log('Session data posted successfully:', response);
+        this.decodedSessionData = response.content || 'No content returned';
+      },
+      error: (error) => {
+        console.error('Error posting session data:', error);
+        this.decodedSessionData = 'Error generating QR code data';
+      }
+    });
   }
 
 }
