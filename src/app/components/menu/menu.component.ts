f02ec82a771c9f1831b6452fc4d849b53a8d4ea4
Gabriel <gabrielgarrone670@gmail.com>
2025-09-16 17:02:10 +0200

    feat: enhance menu item styles and update language labels

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import menuConfigData from "../../../assets/data/menu-config.json";
 import { GameSessionService } from "../../services/game-session.service";
-import { routes } from "../../app.routes";
 import { Router } from "@angular/router";
 import { LanguageService } from "../../services/language.service";
 
