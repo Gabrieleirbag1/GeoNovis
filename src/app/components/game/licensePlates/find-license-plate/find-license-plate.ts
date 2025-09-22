a4f7e205b60a7f79817bcefada338629ad3c2d37
Gabriel <gabrielgarrone670@gmail.com>
2025-09-22 14:59:57 +0200

    feat: implement find license plate component with styling and tests

---
+++ src/app/components/game/licensePlates/find-license-plate/find-license-plate.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, OnChanges, Input, SimpleChanges, Output, EventEmitter } from '@angular/core';
+import { GameService } from '../../../../services/game.service';
+import { Country } from '../../../../types/country.type';
+import { CommonModule } from '@angular/common';
+import { ConvertService } from '../../../../services/convert.service';
+import { CountryCode } from '../../../../types/code.type';
+import { CountryInfo } from '../../../../types/country-info.type';
+
+
+@Component({
+  selector: 'app-find-license-plate',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './find-license-plate.html',
+  styleUrls: ['./find-license-plate.css', '../../game/game.component.css'],
+})
+export class FindLicensePlate implements OnInit, OnChanges {
+  countries: Country[] = [];
+  selectedCountry: string = '';
+
+  @Input() turn!: number; // new input to track round changes
+
+  @Output() answerSelected = new EventEmitter<{selectedCode: CountryCode, correctCode: CountryCode}>();
+
+  constructor(
+    private gameService: GameService, 
+    private convertService: ConvertService
+  ) {}
+
+  ngOnInit(): void {
+    this.init();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['turn'] && !changes['turn'].isFirstChange()) {
+      this.init();
+    }
+  }
+
+  onAnswerSelect(country: Country): void {
+    this.answerSelected.emit({
+      selectedCode: country.code,
+      correctCode: this.gameService.selectedCountryCode
+    });
+  }
+
+  init(): void {
+    // console.log('FindPlate Component Initialized');
+    this.gameService.initializeGame(6);
+    this.countries = this.gameService.getCountries();
+    const countryInfo: CountryInfo | null = this.convertService.convertCodeToCountry(this.gameService.selectedCountryCode)
+    this.selectedCountry = countryInfo?.country[this.convertService.language] || '';
+  }
+
+  getPlateImage(countryCode: string): string {
+    return '/images/plates/' + countryCode.toLowerCase() + '.svg';
+  }
+
+}
