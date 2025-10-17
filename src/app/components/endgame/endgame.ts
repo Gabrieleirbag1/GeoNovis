b47ec0fb09ef8caed34f9345394d26b2258f7021
Gabriel <gabrielgarrone670@gmail.com>
2025-10-17 10:02:20 +0200

    feat: implement endgame component with styling and game state display

--- src/app/components/endgame/endgame.ts
+++ src/app/components/endgame/endgame.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { GameSessionService } from '../../services/game-session.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-endgame',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './endgame.html',
   styleUrl: './endgame.css'
 })
 export class Endgame {
+  countries: any[] = [];
+  roundsCompleted: number = 0;
+  constructor(private gameSessionService: GameSessionService) {}
 
+  ngOnInit(): void {
+    const gameState = this.gameSessionService.getGameState();
+    for (const key in gameState) {
+      if (gameState[key].hasOwnProperty('right')) {
+        this.countries.push(gameState[key]);
+        this.roundsCompleted++;
+      }
+    }
+  }
 }
