85db24dd297efae1af37769cc7ec51156a4b764d
Gabriel <gabrielgarrone670@gmail.com>
2025-10-22 09:42:03 +0200

    feat: add loading guard to manage loader visibility during route activation

---
+++ src/app/guards/loading.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { LoadingService } from '../services/loading.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingGuard implements CanActivate {
+  constructor(
+    private loadingService: LoadingService
+  ) {}
+
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    this.loadingService.showLoader();
+    return true;
+  }
+}
